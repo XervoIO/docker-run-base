b66d323663edde72ae3a7ee8802c4ac9d6628e56
Brandon Cannaday <brandon@modulus.io>
2015-04-09 16:08:52 -0400

    Added supervisor service and base config.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,10 @@ set -x
 
 apt-get update
 apt-get install -y supervisor git
+
+# Copy supervisor's base configuration.
+cp /opt/modulus/supervisor.conf /etc/supervisor/conf.d/
+
+# Setup supervisor's service.
+mkdir /etc/service/supervisor/
+cp /opt/modulus/supervisor-service.sh /etc/service/supervisor/run
