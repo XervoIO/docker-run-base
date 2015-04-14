da7982c59b585333258de31d036bb41f35b101f1
Brandon Cannaday <brandon@modulus.io>
2015-04-14 17:09:29 -0400

    Added s3fs binary.

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,11 @@ cp /opt/modulus/supervisord.conf /etc/supervisor/
 # Setup supervisor's service.
 mkdir /etc/service/supervisor/
 cp /opt/modulus/supervisor-service.sh /etc/service/supervisor/run
-chmod +x /etc/service/supervisor/run
+
+# Rename versioned navi to just navi.
 mv /opt/modulus/bin/navi* /opt/modulus/bin/navi
+
+# Make things executable.
+chmod +x /etc/service/supervisor/run
 chmod +x /opt/modulus/bin/navi
+chmod +x /opt/modulus/bin/s3fs
